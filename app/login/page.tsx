d98f0ed12ffaf300d6316287ea9510d5c22ba2ff
Matej7418 <matejsl404@gmail.com>
2023-05-12 19:41:20 +0200

    login styl

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -29,18 +29,28 @@ export default function About() {
             </Head>
             <p className="text-center mt-10 font-medium text-gray-500">Prihlasovanie</p>
             <p className="text-center mt-24 text-xl font-bold tracking-tighter">Vitajte v appke GoldLux!</p>
-            <span className="p-float-label mt-16">
-                <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <label htmlFor="email">Email</label>
-            </span>
+            <div className="" >
+                <div style={centerH} className="mt-16">
+                <span className="p-float-label">
+                    <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <label htmlFor="email">Email</label>
+                </span>
+                </div>
+                <div style={centerH} className="mt-[135px]">
+                <span className="p-float-label">
+                    <Password inputId="password" value={password} onChange={(e) => setPassword(e.target.value)} toggleMask
+                              promptLabel="Zadajte heslo" weakLabel="Slabé" mediumLabel="Priemerné" strongLabel="Silné"/>
+                    <label htmlFor="password">Heslo</label>
+                </span>
+                </div>
+                <div style={centerH}>
+                    <Button style={{marginTop: "48vh"}} label="Prihlásiť sa" severity="secondary" outlined />
+                </div>
+                <div style={centerH}>
+                    <p style={{marginTop: "68vh"}} className="mt-20 text-center underline text-gray-600">Alebo, ak ste nový užívateľ, zaregistrujte sa </p>
+                </div>
 
-            <span className="p-float-label mt-5">
-                <Password inputId="password" value={password} onChange={(e) => setPassword(e.target.value)} toggleMask
-                          promptLabel="Zadajte heslo" weakLabel="Slabé" mediumLabel="Priemerné" strongLabel="Silné"/>
-                <label htmlFor="password">Heslo</label>
-            </span>
-            <Button style={{marginTop: "20vh"}} label="Prihlásiť sa" severity="secondary" outlined />
-            <p className="mt-20 text-center underline text-gray-600">Alebo, ak ste nový užívateľ, zaregistrujte sa </p>
+            </div>
         </>
     )
 }
