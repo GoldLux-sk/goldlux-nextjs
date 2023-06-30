6f83419e3a7cf4b646f35404bd781eee0cd77b6c
Matej7418 <matejsl404@gmail.com>
2023-06-30 17:46:50 +0200

    add time

--- app/order/[id]/page.tsx
+++ app/order/[id]/page.tsx
@@ -7,6 +7,8 @@ import { useRouter, redirect } from 'next/navigation'
 import Image from "next/image";
 import Modal from 'react-modal';
 import {cookies} from "next/headers";
+import CancelOrder from "@/components/common/modal/CancelOrder";
+import AddTime from "@/components/common/modal/AddTime";
 
 export default function Order({ params }: {
   params: { id: string }
@@ -56,8 +58,8 @@ export default function Order({ params }: {
 
   const [selectedDay, setSelectedDay] = useState<DayValue>(dateToObj(order.start_end_date));
 
-  const [addIsOpen, setAddOpen] = useState<boolean>(false);
-  const [cancelIsOpen, setCancelOpen] = useState<boolean>(false);
+  const [isAddOpen, setAddOpen] = useState<boolean>(false);
+  const [isCancelOpen, setCancelOpen] = useState<boolean>(false);
   Modal.setAppElement('#root');
 
   const [startTime, setStartTime] = useState<number>(0);
@@ -72,6 +74,8 @@ export default function Order({ params }: {
   const [reset, setReset] = useState<boolean>(true);
   const [resetPause, setResetPause] = useState<boolean>(false);
   const [r2, setR2] = useState<boolean>(false);
+  const [addedTime, setAddedTime] = useState<number>(0);
+  const [updateAdded, setUpdateAdded] = useState<boolean>(false);
 
   useEffect(() => {
     let interval: NodeJS.Timer;
@@ -85,7 +89,7 @@ export default function Order({ params }: {
         setDiffTime(0);
         setPauseTime(0);
         setTotalPauseTime(0);
-        setTotalTime(0);
+        setTotalTime(addedTime);
         setRunning(true);
       }
       interval = setInterval(function() {
@@ -106,7 +110,7 @@ export default function Order({ params }: {
           if(pauseTime != 0) {
             if(pauseTime > 0) setTotalPauseTime(totalPauseTime + pauseTime);
             setPauseTime(0);
-          } else setTotalTime(diffTime - totalPauseTime);
+          } else setTotalTime(diffTime - totalPauseTime + addedTime);
           if(!resetPause) setResetPause(true);
         }
       }, 100);
@@ -117,11 +121,15 @@ export default function Order({ params }: {
           setPauseTime(0);
         }
         if(!resetPause) setResetPause(true);
-        setTotalTime(diffTime - totalPauseTime);
+        setTotalTime(diffTime - totalPauseTime + addedTime);
         if(reset) setR2(false);
         setReset(true);
       }
       if(!running) setRunning(true);
+      if(updateAdded) {
+        setUpdateAdded(false);
+        setTotalTime(diffTime - totalPauseTime + addedTime);
+      }
       // @ts-ignore
       clearInterval(interval);
     }
@@ -132,7 +140,7 @@ export default function Order({ params }: {
     console.log(" ");
     */
     return () => clearInterval(interval);
-  }, [startTime, currentTime, pausedOnTime, diffTime, pauseTime, totalPauseTime, totalTime, timer, running, reset, resetPause, r2]);
+  }, [startTime, currentTime, pausedOnTime, diffTime, pauseTime, totalPauseTime, totalTime, timer, running, reset, resetPause, r2, addedTime, updateAdded]);
 
   function startTimer() {
     if(!timer) setTimer(true);
@@ -141,8 +149,9 @@ export default function Order({ params }: {
   function pauseTimer() { setRunning(false) }
   function endTimer() { setTimer(false) }
 
-  /** 0: paused, 1: running, 2: stopped */
-  const bgCol: string[] = ['gray-100', 'green-100', 'amber-100'];
+  function bgCol(): string {
+    return !running ? 'bg-amber-100' : timer ? 'bg-green-100' : 'bg-gray-100'
+  }
 
   //TODO
   function submitOrder() {
@@ -151,7 +160,15 @@ export default function Order({ params }: {
 
   // TODO
   function cancelOrder() {
-    router.push("/orders/");
+    router.push("/orders");
+  }
+
+  //TODO
+  function addTime(time: number) {
+    setUpdateAdded(true);
+    setAddedTime(Math.max(0, Math.min(24*3600*1000, time)));
+    setTotalTime(diffTime - totalPauseTime + addedTime);
+    console.log(addedTime);
   }
 
   return (
@@ -164,7 +181,7 @@ export default function Order({ params }: {
         <div className="w-6 h-6"/>
       </div>
 
-      {!addIsOpen && !cancelIsOpen ?
+      {!isAddOpen && !isCancelOpen ?
         <div className="mt-3 px-3 flex flex-col items-center">
           <Calendar
             calendarClassName="responsive-calendar"
@@ -176,7 +193,6 @@ export default function Order({ params }: {
         </div>
         : null
       }
-
 
       <div className="mt-5 mx-5 grid grid-cols-4 gap-4">
         <p className="col-span-2 font-semibold">Odhadovaný čas:</p>
@@ -200,7 +216,7 @@ export default function Order({ params }: {
       </div>
 
       <div className="mt-3 flex flex-col justify-center items-center">
-        <div className={`flex flex-col items-center w-[90vw] h-14 bg-${bgCol[!running ? 2 : timer ? 1 : 0]} rounded-2xl border border-neutral-700`}>
+        <div className={`flex flex-col items-center w-[90vw] h-14 ${bgCol()} rounded-2xl border border-neutral-700`}>
           <p className="w-[166px] h-7 text-black text-[40px] font-normal">{formatHMS(new Date(totalTime).toISOString(), true)}</p>
         </div>
         <button type="button" onClick={() => startTimer()} >
@@ -219,7 +235,7 @@ export default function Order({ params }: {
           </button>
         </div>
 
-        <button className="mt-8" type="button" /*onClick={() => setAddOpen(true)}*/>
+        <button className="mt-8" type="button" onClick={() => setAddOpen(true)}>
           <div className="flex flex-row justify-center items-center gap-2 w-[200px] h-12 bg-white rounded-2xl border border border border border-neutral-800">
             <Image src="/plus.svg" alt="Add" width="28" height="28"/>
             <div className="text-black font-normal">PRIDAŤ HODINY</div>
@@ -238,41 +254,8 @@ export default function Order({ params }: {
         </button>
       </div>
 
-      <Modal
-        isOpen={cancelIsOpen}
-        onRequestClose={() => setCancelOpen(false)}
-        contentLabel="Cancel Order"
-        className="custom-modal"
-      >
-        <div className="bg-white w-screen">
-          <h1 className="text-center mt-8 text-black/50 text-md font-bold">{`Objednávka ${params.id}`}</h1>
-          <div className="mx-5 h-[90vh] border border-black rounded-2xl relative">
-            <div className="flex flex-row justify-between p-3">
-              <button type="button" onClick={() => setCancelOpen(false)}>
-                <Image src="/close.svg" alt="Close" width="24" height="24"/>
-              </button>
-              <h1 className="text-black text-md font-bold">Pridať hodiny</h1>
-              <div className="w-6 h-6"/>
-            </div>
-            <hr className="w-full h-px bg-black border-0"/>
-
-            <div className="mt-10 flex flex-col items-center">
-              <div className="grid grid-cols-2 gap-5">
-                <button type="button" onClick={() => setCancelOpen(false)}>
-                  <div className="w-[40vw] h-12 px-5 py-[13px] bg-rose-500 rounded-2xl border border-zinc-800 justify-center items-center gap-2.5">
-                    <div className="text-center text-white text-[17px] font-semibold leading-snug">Naspäť</div>
-                  </div>
-                </button>
-                <button type="button" onClick={() => cancelOrder()}>
-                  <div className="w-full h-12 px-5 py-[13px] bg-rose-500 rounded-2xl border border-zinc-800 justify-center items-center gap-2.5">
-                    <div className="text-center text-white text-[17px] font-semibold leading-snug">Zrušiť</div>
-                  </div>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Modal>
+      <CancelOrder isOpen={isCancelOpen} setOpen={setCancelOpen} id={params.id} cancelOrder={cancelOrder}/>
+      <AddTime isOpen={isAddOpen} setOpen={setAddOpen} id={params.id} time={addedTime} addTime={addTime}/>
 
     </div>
   )
---
+++ components/common/modal/AddTime.tsx
@@ -0,0 +1,133 @@
+'use client'
+
+import React, { FunctionComponent, useState } from "react";
+import Image from "next/image";
+// @ts-ignore
+import Modal from 'react-modal';
+
+type CancelOrderProps = {
+  isOpen: boolean,
+  setOpen: (_: boolean) => void,
+  id: string,
+  time: number,
+  addTime: (time: number) => void,
+}
+
+const CancelOrder: FunctionComponent<CancelOrderProps> = ({
+  isOpen, setOpen, id, time, addTime
+}) => {
+
+  const[seconds, setSeconds] = useState<number>(new Date(time).getUTCSeconds());
+  const[minutes, setMinutes] = useState<number>(new Date(time).getUTCMinutes());
+  const[hours, setHours] = useState<number>(new Date(time).getUTCHours());
+
+  const updateSeconds = (seconds: number) => {
+    let secs = seconds;
+    let mins = 0;
+    while(secs >= 60) {
+      if(hours >= 23 && minutes >= 59) {
+        secs = 59;
+        break;
+      }
+      mins++;
+      secs -= 60;
+    }
+    while(secs < 0) {
+      if(hours <= 0 && minutes <= 0) {
+        secs = 0;
+        break;
+      }
+      mins--;
+      secs += 60;
+    }
+    setSeconds(secs);
+    updateMinutes(minutes + mins);
+  };
+
+  const updateMinutes = (minutes: number) => {
+    let mins = minutes;
+    let hrs = 0;
+    while(mins >= 60) {
+      if(hours >= 23) {
+        mins = 59;
+        return;
+      }
+      hrs++;
+      mins -= 60;
+    }
+    while(mins < 0) {
+      if(hours <= 0) {
+        mins = 0;
+        break;
+      }
+      hrs--;
+      mins += 60;
+    }
+    setMinutes(mins);
+    updateHours(hours + hrs);
+  };
+
+  const updateHours = (hours: number) => {
+    setHours(Math.max(0, Math.min(23, hours)));
+  }
+
+  const handleSeconds = (event: any) => { updateSeconds(parseInt(event.target.value.replace(/^0+/,"")) || 0); }
+  const handleMinutes = (event: any) => { updateMinutes(parseInt(event.target.value.replace(/^0+/,"")) || 0); }
+  const handleHours = (event: any) => { updateHours(parseInt(event.target.value.replace(/^0+/,"")) || 0); }
+
+  function finalTime() {
+    const time = (hours * 3600 * 1000) + (minutes * 60 * 1000) + (seconds * 1000);
+    console.log(time);
+    addTime(time);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setMinutes(0);
+    setHours(0);
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={() => setOpen(false)}
+      contentLabel="Cancel Order"
+      className="custom-modal"
+    >
+      <div className="bg-white w-screen">
+        <h1 className="text-center mt-8 text-black/50 text-md font-bold">{`Objednávka ${id}`}</h1>
+        <div className="mx-5 h-[90vh] border border-black rounded-2xl relative">
+          <div className="flex flex-row justify-between p-3">
+            <button type="button" onClick={() => setOpen(false)}>
+              <Image src="/close.svg" alt="Close" width="24" height="24"/>
+            </button>
+            <h1 className="text-black text-md font-bold">Pridať hodiny</h1>
+            <div className="w-6 h-6"/>
+          </div>
+          <hr className="w-full h-px bg-black border-0"/>
+
+          <div className="mt-10 flex flex-col items-center">
+            <div className="px-5 flex flex-row justify-center items-center gap-1 w-full h-14 bg-gray-100 text-3xl rounded-2xl border border-neutral-700">
+              <input className="w-14" type="number" placeholder="Hours" value={hours} onChange={handleHours}/>
+              <p>:</p>
+              <input className="w-14" type="number" placeholder="Minutes" value={minutes} onChange={handleMinutes}/>
+              <p>:</p>
+              <input className="w-14" type="number" placeholder="Seconds" value={seconds} onChange={handleSeconds}/>
+            </div>
+            <button className="mt-10" type="button" onClick={() => {setOpen(false); finalTime();}}>
+              <Image src="/confirm.svg" alt="Close" width="86" height="86"/>
+            </button>
+            <button className="mt-8" type="button" onClick={() => reset()}>
+              <div className="px-2 flex flex-row justify-center items-center gap-2 h-12 bg-white rounded-2xl border border border border border-neutral-800">
+                <Image src="/close.svg" alt="Add" width="28" height="28"/>
+                <div className="mr-3 text-black font-normal">Vynulovať</div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+export default CancelOrder;
